1a28d99fa63233c7140f312d6033ec1559fe46e5
Mario Huys <woelpad@gmail.com>
2010-09-17 00:10:13 +0900

    Replace ArriveRetreat with RetreatArrive

---
+++ DPjudge/__init__.py
@@ -0,0 +1,27 @@
+#   ===============
+#   DPjudge package
+#   ===============
+import os
+os.sys.path[:0] = [os.path.dirname(os.path.abspath(os.sys.argv[0])) + '/..']
+
+import host
+from Game import Game, Power, Mail, Status
+
+host.packageDir = __path__[0]
+host.dpjudgeDir = host.hostDir + '/web'
+host.gameDir = host.hostDir + '/games'
+
+if __name__ != '__main__': from Page import Page
+
+#   =========================================================================
+#   Synchronize the timestamps we will get from time.time() with NTP service.
+#   -------------------------------------------------------------------------
+if 'ntpService' in dir(host) and host.ntpService:
+    import telnetlib, time
+    try:
+        systime, synch, time.time = time.time, 0.0, lambda: systime() + synch
+        sysNow, ntp = systime(), telnetlib.Telnet(*host.ntpService)
+        for byte in ntp.read_all(): synch = synch * 256 + ord(byte)
+        synch -= 70*365*24*60*60 + 17*24*60*60 + sysNow
+        ntp.close()
+    except: pass
